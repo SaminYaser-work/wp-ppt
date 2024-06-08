e554dd102117c76b1a64ffb8871d69c1d52fbbaf
Samin Yaser <saminyaserwork@gmail.com>
2024-06-08 11:13:13 +0600

    updated metadata

--- presentation.php
+++ presentation.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Plugin Name:       Presentation
- * Description:       A plugin to create presentation
+ * Description:       A plugin to create powerpoint-like presentation
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            Samin Yaser
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       presentation
@@ -22,9 +22,5 @@ define( 'PPT_DIR', plugin_dir_path( __FILE__ ) );
 define( 'PPT_URL', plugin_dir_url( __FILE__ ) );
 
 require_once PPT_DIR . 'includes/class-ppt-init.php';
-
-// add_action('init', function () {
-// 	register_block_type( __DIR__ . '/build' );
-// });
 
 new \Ppt\Ppt_Init();
