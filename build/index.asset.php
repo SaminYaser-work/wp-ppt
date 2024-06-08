0d1f20eb05049482620ec8278e792abe4016c321
Samin Yaser <saminyaserwork@gmail.com>
2024-06-08 11:22:34 +0600

    updated gitignore

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'a678b267ce0b9b0cea40');
--- build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-dom-ready'), 'version' => 'bcb9124dadb1a503b08f');
